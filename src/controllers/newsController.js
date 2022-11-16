0ef5bd459d2f3fac6ad6585a57c8beb786bad41f
Renanppcosta <renanppcosta@gmail.com>
2022-11-16 16:32:17 -0300

    news pagination

--- src/controllers/newsController.js
+++ src/controllers/newsController.js
@@ -24,14 +24,63 @@ const create = async (req,res)=>{
 };
 
 const findAll = async (req,res)=>{
-    const news = await newsService.findAll();
-
-    if(news.length === 0){
-        return res.status(400).send({message:"Não há noticias cadastradas"});
-    }    
-    res.send(news);
+    try {
+        let { limit, offset } = req.query;
     
-}
+        limit = Number(limit);
+        offset = Number(offset);
+    
+        if (!limit) {
+          limit = 5;
+        }
+    
+        if (!offset) {
+          offset = 0;
+        }
+    
+        const news = await newsService.findAll(offset, limit);
+        const total = await newsService.countNews();
+        const currentUrl = req.baseUrl;
+    
+        const next = offset + limit;
+        const nextUrl =
+          next < total ? `${currentUrl}?limit=${limit}&offset=${next}` : null;
+    
+        const previous = offset - limit < 0 ? null : offset - limit;
+        const previousUrl =
+          previous != null
+            ? `${currentUrl}?limit=${limit}&offset=${previous}`
+            : null;
+    
+        if (news.length === 0) {
+          return res.status(400).send({
+            message: "Não há notícias registradas",
+          });
+        }
+        res.send({
+          nextUrl,
+          previousUrl,
+          limit,
+          offset,
+          total,
+    
+          results: news.map((item) => ({
+            id: item._id,
+            title: item.title,
+            text: item.text,
+            banner: item.banner,
+            likes: item.likes,
+            comments: item.comments,
+            name: item.user.name,
+            username: item.user.username,
+            userAvatar: item.user.avatar,
+          })),
+        });
+      } catch (err) {
+        res.status(500).send({ message: err.message });
+      }
+    };
+
 
 
 module.exports = {
--- src/services/newsService.js
+++ src/services/newsService.js
@@ -2,10 +2,13 @@ const News = require("../models/News");
 
 const create = (body) => News.create(body);
 
-const findAll = () => News.find();
+const findAll = (offset, limit) => News.find().sort({_id:-1}).skip(offset).limit(limit).populate("user");
+
+const countNews = () => News.countDocuments();
 
 
 module.exports = {
     create,
-    findAll
+    findAll,
+    countNews
 }
